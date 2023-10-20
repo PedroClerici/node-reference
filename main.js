ee5ff12c574c3418c48d6368669554ded3c62cb8
Pedro Clerici <pedro.clerici@tutamail.com>
2023-10-20 11:03:30 -0300

    Finished 2.2-using-middlewares

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 8000;
 
-app.use((req, res) => {
-  res.send("<h1>Hello, world!</h1>");
+app.use("/", bodyParser.urlencoded({ extended: false }));
+
+app.use(adminRoutes);
+
+app.use("/", (req, res, next) => {
+  console.log("This always runs!");
+  next();
+});
+
+router.post("/product", (req, res) => {
+  console.log(req.body);
+
+  res.redirect("/");
+});
+
+router.get("/add-product", (req, res) => {
+  res.send(`
+    <form action="/product" method="post">
+      <input type="text" name="name" />
+      <button type="submit">Add Product</button>
+    </form>
+  `);
 });
 
 app.listen(port);
